e86b391bbbc90afb418d4115668cefc1d4bf308d
JD <jangu2464@gmail.com>
2024-02-29 17:20:48 +0100

    Refactorizar firma de metodos

--- src/com/miejemplo/utilidades/Calculadora.java
+++ src/com/miejemplo/utilidades/Calculadora.java
@@ -13,44 +13,44 @@ public class Calculadora {
 
     public int resultado;
     
-    public static int sumar(int a, int b) {
-        return a + b;
+    public static int sumar(int sumando1, int sumando2) {
+        return sumando1 + sumando2;
     }
 
-    public static int restar(int a, int b) {
-        return a - b;
+    public static int restar(int restar1, int restar2) {
+        return restar1 - restar2;
     }
 
-    public static int multiplicar(int a, int b) {
-        return a * b;
+    public static int multiplicar(int multiplicar1, int multiplicar2) {
+        return multiplicar1 * multiplicar2;
     }
 
-    public static int dividir(int a, int b) {
-        if (b == 0) {
+    public static int dividir(int dividir1, int dividir2) {
+        if (dividir2 == 0) {
             throw new ArithmeticException("División por cero");
         }
-        return a / b;
+        return dividir1 / dividir2;
     }
 
-    public static int modulo(int a, int b) {
-        if (b == 0) {
+    public static int modulo(int modulo1, int modulo2) {
+        if (modulo2 == 0) {
             throw new ArithmeticException("Módulo por cero");
         }
-        return a % b;
+        return modulo1 % modulo2;
     }
     
-    public static int operacion(int a, int b, String operacion) {
+    public static int operacion(int operador1, int operador2, String operacion) {
         switch(operacion){
             case "sumar":
-                return sumar(a,b);
+                return sumar(operador1,operador2);
             case "restar":
-                return restar(a,b);
+                return restar(operador1,operador2);
             case "multiplicar":
-                return multiplicar(a,b);
+                return multiplicar(operador1,operador2);
             case "dividir":
-                return dividir(a,b);
+                return dividir(operador1,operador2);
             case"modulo":
-                return modulo(a,b);
+                return modulo(operador1,operador2);
             default:        
         throw new IllegalArgumentException("Operación no válida");
         }
